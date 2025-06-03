7072aceeaad7088012678a83ff5a2293bbb3adeb
Incivius <marcos.santos232@fatec.sp.gov.br>
2025-06-03 13:19:26 -0300

    init: initialize project java

---
+++ src/main/java/com/youtool_api/YoutoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.youtool_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutoolApiApplication.class, args);
+	}
+
+}
